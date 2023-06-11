1a74626e689a25f76ef6446313521c0399d5da4e
gabriel <prates_peralta@hotmail.com>
2023-06-11 20:07:53 -0300

    refactor: Improve code by using findIndex and destructuring in deleteUser endpoint

--- teste3.js
+++ teste3.js
@@ -1,15 +1,31 @@
-var data =  require("./fakeData");
+var data = require("./fakeData");
 
-module.exports = function(req, res) {
-  
-    var name =  req.query.name;
+// module.exports = function(req, res) {
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
+//     var name =  req.query.name;
+
+//     for(let i = 0; i < data.length;  i++) {
+//         if(i.name == name) {
+//             data[i] = null;
+//         }
+//     }
+
+//     res.send("success");
+
+// };
+
+module.exports = function (req, res) {
+
+    try {
+        const { name } = req.query;
+        const userIndex = data.findIndex((item) => item.name === name); // encontra o índice do usuário no array
+        if (userIndex === -1) { // se o índice for -1, significa que o usuário não foi encontrado
+            return res.status(404).send("User not found");
         }
+        data.splice(userIndex, 1); // remove o usuário do array
+        return res.status(200).send("User deleted");
+    } catch (error) {
+        return res.status(500).send("Internal Server Error");
     }
-
-    res.send("success");
 
 };
