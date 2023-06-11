d2385c95c04bd78bf9928af3b98b051986b6857b
gabriel <prates_peralta@hotmail.com>
2023-06-11 20:00:57 -0300

    refactor: add error handling and input validation to create user endpoint

--- teste2.js
+++ teste2.js
@@ -1,17 +1,25 @@
-var data =  require("./fakeData");
+var data = require("./fakeData");
 
-module.exports = function(req, res){
-  
-    var name =  req.body.name;
-    var jov =  req.body.job;
-    
-    var newUser = {
-        name: name,
-        job: job,
+module.exports = function (req, res) {
+    try {
+        // desestruturando o corpo da requisição para evitar a criação de uma variável desnecessária
+        const { name, job } = req.body;
+
+        // validando o corpo da requisição para evitar que o usuário seja criado com dados inválidos
+        if (!name || !job) {
+            return res.status(400).send("Name and job are required");
+        }
+
+        const newUser = {
+            name,
+            job,
+        }
+
+        data.push(newUser);
+
+        return res.status(201).send(newUser);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Internal Server Error");
     }
-
-    data.push(newUser)
-    
-    res.send(newUser);
-
 };
